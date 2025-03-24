119cbf7344fa102214c3da6aa15879514b2e0cbf
harsh <harshv339853gmail.com>
2025-03-24 18:43:21 +0530

    band name generator PROHECT

---
+++ project band name generator.py
@@ -0,0 +1,6 @@
+print ("WLECOME TO BAND NAME GENERATOR")
+City = input("WHICH CITY YOU GROW UP IN \n")
+print(City)
+pet = input("WHAT IS THE PET NAME \n")
+print(pet)
+print ("YOUR BANED NAME COULD BE:-" + City +" "+ pet)
