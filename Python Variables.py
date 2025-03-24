81889d579f2e2e6efd3cf02e8d9dc889973cb32c
harsh <harshv339853gmail.com>
2025-03-24 13:20:24 +0530

    name\input\print(len(a))

---
+++ Python Variables.py
@@ -0,0 +1,26 @@
+# name=input("what is your name")
+# print(name)
+
+name="harsh"
+print(name)
+print(len(name))
+
+name1="yash"
+print(name1)
+print(len(name1)) #this code find to length of string
+
+name= input("what is your name?")
+print(name)
+print(len(name))
+
+
+name=("hello"+input("what is your name"))
+print(name)
+print(len(name))
+
+# in one line code to calculate length string
+print(len(input("WHAT IS YOUR username?")))
+
+username = input("WHAT IS YOUR username?")
+length = print(len(username))
+#  
