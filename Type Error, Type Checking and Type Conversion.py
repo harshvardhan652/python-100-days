dfaf07bd281ea3cd5bae847ff43dca3b0c40b58c
harsh <harshv339853gmail.com>
2025-03-25 22:50:56 +0530

    type and solving error

---
+++ Type Error, Type Checking and Type Conversion.py
@@ -0,0 +1,26 @@
+# len("hello")
+
+print (type("hello"))
+print(type("123"))
+
+print(type(122))
+print(type(12+12))
+
+print(type(12.20))
+print(type(12.36 + 45))
+
+print(type(True))
+print(type(False))
+
+print(int ("123") + int("456"))
+print(float(12.00) + float(12.01))
+
+# SOLVE Error 
+# print("Number of letters in your name: " + len(input("Enter your name")))
+# print("number of letters in your name" + len(input("enter your name")))
+
+# print("Number of letters in your name: " + len(input("Enter your name")))
+
+
+print("number of letters in your name")
+print(len(input("ENTER YOUR NAME:-\n")))
