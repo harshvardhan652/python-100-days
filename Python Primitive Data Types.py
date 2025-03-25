bf406b01e8d76d6fbe60486cb8e186fe437c10fe
harsh <harshv339853gmail.com>
2025-03-25 22:38:51 +0530

    subscripting,integer,float,boolean,etc

---
+++ Python Primitive Data Types.py
@@ -0,0 +1,29 @@
+#subscripting
+print("hello"[0])
+print("hello"[1])
+print("hello"[2])
+print("hello"[3])
+print("hello"[4])
+
+print("hello"[-1])
+print("hello"[-2])
+print("hello"[-3])
+print("hello"[-4])
+print("hello"[-5])
+
+#string
+print("1234"+"9874")
+
+#INTEGER = WHOLE NUMBER
+print(123 + 1235)
+
+#LARGE INTEGER
+print(123456789)
+
+#float = floating point number
+3.741
+
+#boolean
+print(True)
+print(False)
+
